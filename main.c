1db6e43c6a783b25fa447ebab77be20e0b5349a1
klopp <klopp@yandex.ru>
2016-06-19 20:40:13 +0300

    main.picoc.h

--- main.c
+++ main.c
@@ -4,30 +4,25 @@
  */
 
 #include "picoc-lib.h"
+#include "./t/main.picoc.h"
 
 int main() {
     PicoCLib pc;
     int rc;
     int aaa = 9;
     char bbb[16] = { 'b', 'B', 0 };
-    struct {
-        int a;
-        int b;
-    } ab = { 33, 44 };
-    struct {
-        int a;
-        int b;
-    } ad = { 55, 66 };
+    ab a = { 33, 44 };
+    ab b = { 55, 66 };
     PicoCLibInit( &pc );
     PicoCLibBindInt( &pc, "aaa", &aaa );
     PicoCLibBindCharArray( &pc, "bbb", bbb );
-    PicoCLibBindArray( &pc, "ccc", &ab );
-    PicoCLibBindArray( &pc, "ddd", &ad );
+    PicoCLibBindArray( &pc, "ccc", &a );
+    PicoCLibBindArray( &pc, "ddd", &b );
     rc = PicoCLibMain( &pc, "./t/main.picoc", NULL );
     printf( "rc = %d, exit value: %d, error:\n%s\n", rc, pc.pc.PicocExitValue,
             pc.PicocOutBuf );
-    printf( "aaa: %d, bbb: %s, [%d:%d], [%d:%d]\n", aaa, bbb, ab.a, ab.b,
-            ad.a, ad.b );
+    printf( "aaa: %d, bbb: %s, [%d:%d], [%d:%d]\n", aaa, bbb, a.a, a.b,
+            b.a, b.b );
     PicoCLibDown( &pc );
     return 0;
 }
