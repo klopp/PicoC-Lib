f05cc8916d20e43c45ba15ba008fa380aa98efa4
klopp <klopp@yandex.ru>
2016-06-19 14:52:11 +0300

    created

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ *  Created on: 19 июня 2016 г.
+ *      Author: Vsevolod Lutovinov <klopp@yandex.ru>
+ */
+
+#include "../picoc/picoc.h"
+
+int main() {
+    return 0;
+}
+
+/*
+ *  That's All, Folks!
+ */
+
