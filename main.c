dde3147b1e11ecc0d1d01c9981f5e7b5a70a64f3
klopp <klopp@yandex.ru>
2016-06-20 02:26:29 +0300

    file vars cleanup

--- main.c
+++ main.c
@@ -6,6 +6,25 @@
 #include "picoc-lib.h"
 #include "./t/main.picoc.h"
 
+void TableDeleteFromFile( Picoc *pc, struct Table *Tbl, const char *Key ) {
+    struct TableEntry **EntryPtr;
+    short i;
+    for( i = 0; i < Tbl->Size; i++ ) {
+        EntryPtr = &Tbl->HashTable[i];
+        while( EntryPtr && *EntryPtr ) {
+            if( ( *EntryPtr )->DeclFileName
+                    && !strcmp( ( *EntryPtr )->DeclFileName, Key ) ) {
+                struct TableEntry *DeleteEntry = *EntryPtr;
+                *EntryPtr = DeleteEntry->Next;
+                HeapFreeMem( pc, DeleteEntry );
+            }
+            else {
+                EntryPtr = &( *EntryPtr )->Next;
+            }
+        }
+    }
+}
+
 int main() {
     PicoCLib pc;
     int rc;
@@ -21,8 +40,16 @@ int main() {
     rc = PicoCLibMainFromFiles( &pc, "./t/main.picoc", NULL );
     printf( "rc = %d, exit value: %d, error:\n%s\n", rc, pc.pc.PicocExitValue,
             pc.PicocOutBuf );
-    printf( "aaa: %d, bbb: %s, [%d:%d], [%d:%d]\n", aaa, bbb, a.a, a.b,
-            b.a, b.b );
+    printf( "aaa: %d, bbb: %s, [%d:%d], [%d:%d]\n", aaa, bbb, a.a, a.b, b.a,
+            b.b );
+    TableDeleteFromFile( &pc.pc, &pc.pc.GlobalTable, "./t/main.picoc.h" );
+    TableDelete( &pc.pc, &pc.pc.GlobalTable, TableStrRegister( &pc.pc, "main" ) );
+    TableDelete( &pc.pc, &pc.pc.GlobalTable, TableStrRegister( &pc.pc,
+                 "__exit_value" ) );
+    aaa = 6;
+    rc = PicoCLibMainFromFiles( &pc, "./t/main.picoc", NULL );
+    printf( "rc = %d, exit value: %d, error:\n%s\n", rc, pc.pc.PicocExitValue,
+            pc.PicocOutBuf );
     PicoCLibDown( &pc );
     return 0;
 }
