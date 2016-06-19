789134b6f102a5954358f753d92a1b3d45b52223
klopp <klopp@yandex.ru>
2016-06-19 18:31:21 +0300

    bindings

--- main.c
+++ main.c
@@ -6,6 +6,23 @@
 #include "picoc-lib.h"
 
 int main() {
+    PicoCLib pc;
+    int rc;
+    int aaa = 9;
+    char bbb[16] = { 'b', 'B', 0 };
+    PicoCLibInit( &pc );
+    PicoCLibBindInt( &pc, "aaa", &aaa );
+    PicoCLibBindCharArray( &pc, "bbb", bbb );
+    rc = PicoCLibMain( &pc, "./t/main.picoc" );
+    printf( "rc = %d, exit value: %d, error:\n%s\n", rc, pc.pc.PicocExitValue,
+            pc.PicocOutBuf );
+    /*
+        PicoCLibReset( &pc );
+        rc = PicoCLibMain( &pc, "./t/main.picoc" );
+        printf( "rc = %d, exit value: %d, error:\n%s\n", rc, pc.pc.PicocExitValue,
+                pc.PicocOutBuf );
+    */
+    PicoCLibDown( &pc );
     return 0;
 }
 
